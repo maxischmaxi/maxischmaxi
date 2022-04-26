10344ce31d5f6be53c2dbb7603bff5224d0f03fa
maxischmaxi <maxjeschek@bstreet.de>
2022-04-26 12:57:14 +0200

    install script

--- .zsh-install/install.sh
+++ .zsh-install/install.sh
@@ -1,2 +1,6 @@
+# install oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# install syntax highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+# install robbyrussel
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
